fcd7d4db367cdd20bf80cb294cc5c319d08119e7
L Daniel Swakman <d.swakman@gmail.com>
2016-12-30 16:25:56 +0300

    cleaned up header

--- header.php
+++ header.php
@@ -11,36 +11,25 @@
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link href="https://fonts.googleapis.com/css?family=Andada" rel="stylesheet">
-<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 0.5" />
 
-<?php wp_head(); ?>
+<head>
+
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" rel="stylesheet" />
+
+	<?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 
 
-	<header id="masthead" class="site-header" role="banner">
-
-		<div id="headerBrush">
-
-			<img id="" src="<?php bloginfo('template_directory') ?>/img/brush-header.png">
-		</div>
-		
-		<div class="site-branding">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><span id='siteLogo'></span></a></h1>
-
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'elifbas' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	<nav>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo"><span id='siteLogo'></span></a>
+		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container_class' => 'menu-wrapper' ) ); ?>
+	</nav>
 
 	<div id="content" class="site-content">
