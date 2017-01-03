fdfb27d3b433b8abda2e5c9bd7b327e17c15cfe4
L Daniel Swakman <d.swakman@gmail.com>
2017-01-03 09:36:22 +0300

    temporarily restored old webfont

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Andada" rel="stylesheet">
 
 	<?php wp_head(); ?>
 
